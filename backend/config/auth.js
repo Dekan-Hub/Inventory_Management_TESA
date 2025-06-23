23332d26b85d738064152071f91998120179f4ae
Dekan-Kun <appsandplays@hotmail.com>
2025-06-23 18:20:20 -0500

    cambios

--- backend/config/auth.js
+++
@@ -1,5 +0,0 @@
-// backend/config/auth.js
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'tu_secreto_super_secreto', // ¡Cambia esto en producción!
-  jwtExpiresIn: process.env.JWT_EXPIRES_IN || '1d' // El token expira en 1 día
-};
